ba5edc91ba3867e7fe3953cd3655d3717d6278e4
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 16:47:39 +0530

    Finish 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+    let tempInCelcius;
+    tempInCelcius = (tempInFahrenheit-32) * 5/9;
+    tempInCelcius = Math.round(tempInCelcius * 10) / 10;
+
+    return tempInCelcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+    let tempInFahrenheit;
+    tempInFahrenheit = tempInCelcius * 9/5 + 32;
+    tempInFahrenheit = Math.round(tempInFahrenheit * 10) / 10;
+
+    return tempInFahrenheit
 };
 
 // Do not edit below this line
