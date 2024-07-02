42a2a4b55343bd7cff3fdffd8467bfbd097ad7fa
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 15:57:30 +0530

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    
+    if(start < 0 || end < 0)    return "ERROR";
+    if(typeof(start)!=='number' || typeof(end)!=='number')      return "ERROR";
+    if(start>end) {
+        const temp = start
+        start = end;
+        end = temp;
+    }
+    
+    let sum = 0;
+    for(let number=start; number<=end; number++) {
+        sum += number;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
