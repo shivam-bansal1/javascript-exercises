46c0da450538778a013cb716524b23002a93f7d7
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 15:21:17 +0530

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToBeRemoved) {
+    // for(let index=0; index<arr.length; index++) {
+    //     if(arr[index] === elementToBeRemoved) {
+    //         arr.splice(index, 1);
+    //     }
+    // }
+
+    // return arr;
+    return arr.filter(element => !elementsToBeRemoved.includes(element));
 };
 
 // Do not edit below this line
