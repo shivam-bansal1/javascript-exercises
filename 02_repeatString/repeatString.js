5279fefff1e4a8d80307c396060c01ae88fb4bd9
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 09:15:30 +0530

    Finish 02_repeatStrings exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0) 
+        return "ERROR";
 
+    let resultString = '';
+    for(let i=0;i<num;i++) {
+        resultString += str;
+    }
+
+    return resultString;
 };
 
 // Do not edit below this line
