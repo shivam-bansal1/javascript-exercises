6190f5fb0ef069153a2728ae82338775b5e79f33
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 16:04:32 +0530

    Finish 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function(yearToCheck) {
+    if((yearToCheck%4===0 && yearToCheck%100 !==0) || yearToCheck%400 === 0)
+        return true;
 
+    return false;
 };
 
 // Do not edit below this line
