6797af74ea29d611e9fbc3082edfcf25410a02e1
Shivam Bansal <shivambnsal@gmail.com>
2024-07-02 10:40:49 +0530

    Finish 03_reverseStrings exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedString = '';
+    for(i=str.length-1; i>=0; i--) {
+        reversedString += str.charAt(i);
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line
