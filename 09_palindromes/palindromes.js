d1d5fc0e75d40dea018b29584faa1377d84633fd
Shivam Bansal <shivambnsal@gmail.com>
2024-07-11 21:02:54 +0530

    Finish 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+function removePunctuation(str) {
+    return str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+}
 
+const palindromes = function (str) {
+    str = removePunctuation(str);
+    for(let i=0,j=str.length-1;i<str.length,j>=0;i++,j--) {
+        if(str.charAt(i).toLowerCase() != str.charAt(j).toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
