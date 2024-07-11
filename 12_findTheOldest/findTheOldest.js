c1b9b68eb4d5b2ad1066c460310d1c852440ceec
Shivam Bansal <shivambnsal@gmail.com>
2024-07-11 21:46:17 +0530

    Finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function findAge(deathYear, birthYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear-birthYear;
+}
 
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, currentPerson) => {
+        const currentPersonAge = findAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        const oldestPersonAge = findAge(oldest.yearOfDeath, oldest.yearOfBirth);
+
+        return oldestPersonAge < currentPersonAge ? currentPerson: oldest;
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
