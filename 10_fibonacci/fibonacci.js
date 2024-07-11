86d6cfcbb89d42c0cf23f69eb97cb75c697a6ba4
Shivam Bansal <shivambnsal@gmail.com>
2024-07-11 21:19:27 +0530

    Finish 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    
+    if (num < 0)
+        return "OOPS";
+    if (num === 0 || num === 1)
+        return num;
 
+
+    let first = 0;
+    let second = 1;
+
+    for(let i=0; i<num-1; i++) {
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
