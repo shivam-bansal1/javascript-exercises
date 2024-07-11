1159310023fb5ad7dbd22f431587d49607e6c61e
Shivam Bansal <shivambnsal@gmail.com>
2024-07-11 20:24:54 +0530

    Finish 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) {
+    return 0;
+  }
+  
+  return arr.reduce((total, currentNumber) => total+currentNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => num*product);
 };
 
-const power = function() {
-	
+const power = function(base, powerNum) {
+	return Math.pow(base, powerNum);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  for(let i=1; i<=num ;i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
